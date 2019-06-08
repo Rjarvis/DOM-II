d80b53b026666e6ce9afc6220e47f07a53712820
Roland J Jarvis <Rjarvis87@gmail.com>
2019-06-08 14:21:20 -0400

    got colors changed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+//DOM objects
+const navBtns = document.querySelectorAll('.nav a');
+const gamePad = new GamepadEvent('true');
+
+console.log(gamePad);
+
+navBtns.forEach(function(i){
+    i.addEventListener('mouseenter',(event)=>{
+        changeColor(i, 'green');
+        event.target.style.color = 'white';
+    });
+    i.addEventListener('mouseleave',(event)=>{
+        changeColor(i, null);
+        event.target.style.color = 'black';
+    })
+    i.addEventListener('keydown',(event)=>{
+        alert(`you clicked on the ${event.target.textContent} button`);
+    })
+})
+
+function changeColor(obj, color){
+    obj.style.backgroundColor = color;
+}
